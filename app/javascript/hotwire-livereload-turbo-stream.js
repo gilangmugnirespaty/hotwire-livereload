8e08e0ca49b49ce2fd7e84aeb5f8a716841b9909
Gilang Mugni Respaty <gilmoregarland@gmail.com>
2023-07-11 13:34:28 +0700

    presist scroll

--- app/javascript/hotwire-livereload-turbo-stream.js
+++ app/javascript/hotwire-livereload-turbo-stream.js
@@ -3,25 +3,18 @@ import received from "./lib/hotwire-livereload-received"
 (() => {
   if(window.HotwireLivereload){ return; }
 
-  var lastScrollTopPosition = null;
-
+  window.HotwireLiveReloadLastScrollTopPosition = null;
   window.HotwireLivereload = function({ target }) {
     const element = target.querySelector('template')?.content.getElementById('hotwire-livereload')
 
     if (element) {
-      lastScrollTopPosition = document.documentElement.scrollTop;
-
+      window.HotwireLiveReloadLastScrollTopPosition = document.documentElement.scrollTop;
       received({ force_reload: element.dataset.forceReload })
     }
   };
 
   document.addEventListener('turbo:before-stream-render', window.HotwireLivereload);
   document.addEventListener('turbo:load', function() {
-    console.log(lastScrollTopPosition, 'lastScrollTopPosition')
-    if (lastScrollTopPosition) {
-      document.documentElement.scrollTo(0, lastScrollTopPosition)
-      lastScrollTopPosition = null
-    }
+    document.documentElement.scrollTo(0, window.HotwireLiveReloadLastScrollTopPosition)
   });
 })();
-
